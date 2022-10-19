791c3d4cac1bce0eb4e9bcca18a18923bf27d928
Simon Rofe <simon.rofe@ieee.org>
2022-10-19 12:56:58 +1000

    Initial Commit

---
+++ Snacks/SnacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnacksApp.swift
+//  Snacks
+//
+//  Created by Simon Rofe on 19/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct SnacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
